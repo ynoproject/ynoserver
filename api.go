e80254d051fb61517b5583e547ed34b07c8b1c1e
kekami <kekami@kekami.dev>
2022-05-27 14:46:00 -0400

    Move some party-related code into party.go

--- api.go
+++ api.go
@@ -18,34 +18,6 @@ type PlayerInfo struct {
 	BadgeSlotRows int    `json:"badgeSlotRows"`
 }
 
-type Party struct {
-	Id          int           `json:"id"`
-	Name        string        `json:"name"`
-	Public      bool          `json:"public"`
-	Pass        string        `json:"pass"`
-	SystemName  string        `json:"systemName"`
-	Description string        `json:"description"`
-	OwnerUuid   string        `json:"ownerUuid"`
-	Members     []PartyMember `json:"members"`
-}
-
-type PartyMember struct {
-	Uuid          string `json:"uuid"`
-	Name          string `json:"name"`
-	Rank          int    `json:"rank"`
-	Account       bool   `json:"account"`
-	Badge         string `json:"badge"`
-	SystemName    string `json:"systemName"`
-	SpriteName    string `json:"spriteName"`
-	SpriteIndex   int    `json:"spriteIndex"`
-	MapId         string `json:"mapId"`
-	PrevMapId     string `json:"prevMapId"`
-	PrevLocations string `json:"prevLocations"`
-	X             int    `json:"x"`
-	Y             int    `json:"y"`
-	Online        bool   `json:"online"`
-}
-
 func initApi() {
 	http.HandleFunc("/api/admin", handleAdmin)
 	http.HandleFunc("/api/party", handleParty)
