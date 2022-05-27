fc17c96f85f15ef6f4a5b160ffdb89f2185f1ea5
kekami <kekami@kekami.dev>
2022-05-27 04:00:50 -0400

    Fix bug caused by previous commit

--- database.go
+++ database.go
@@ -27,7 +27,8 @@ func readPlayerData(ip string) (uuid string, rank int, banned bool, muted bool) 
 	if err != nil {
 		if err == sql.ErrNoRows {
 			uuid = randstr.String(16)
-			createPlayerData(ip, uuid, 0, isVpn(ip))
+			banned = isVpn(ip)
+			createPlayerData(ip, uuid, 0, banned)
 		} else {
 			return "", 0, false, false
 		}
