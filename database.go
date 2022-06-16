e04b6b5e39e63a22147ac890b45beeb4ff7f21f0
Flashfyre <flashfireex@gmail.com>
2022-06-16 11:22:41 -0400

    Remove game check from badge deletion query

--- database.go
+++ database.go
@@ -1050,7 +1050,7 @@ func tryCompletePlayerEventLocation(periodId int, playerUuid string, location st
 }
 
 func writeGameBadges() (err error) {
-	_, err = db.Exec("DELETE FROM badges WHERE game = ?", config.gameName)
+	_, err = db.Exec("DELETE FROM badges")
 	if err != nil {
 		return err
 	}
