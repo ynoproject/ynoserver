3a508e3604d871dd800cc459cd3938f17bee6665
azarashi <azarashi@majestaria.fun>
2022-09-05 17:14:22 -0400

    The power of :=

--- hub.go
+++ hub.go
@@ -100,7 +100,7 @@ func (h *Hub) run() {
 			var account bool
 
 			if conn.Token != "" {
-				uuid := readUuidFromToken(conn.Token)
+				uuid = readUuidFromToken(conn.Token)
 				if uuid != "" { //if we got a uuid back then we're logged in
 					account = true
 				}
