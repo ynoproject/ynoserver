c1bc3c9b8ca8f34abc413e412d261adcf9db876d
azarashi <azarashi@majestaria.fun>
2022-09-12 21:51:07 -0400

    Increase max save size

--- api.go
+++ api.go
@@ -634,7 +634,7 @@ func handleSaveSync(w http.ResponseWriter, r *http.Request) {
 		}
 		data, err := io.ReadAll(r.Body)
 		defer r.Body.Close()
-		if err != nil || len(data) > 1024 * 1024 * 2 {
+		if err != nil || len(data) > 1024 * 1024 * 4 {
 			handleError(w, r, "invalid data")
 			return
 		}
