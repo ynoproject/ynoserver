fe1086a74396f96b8eb977e2e7609e1537237d6c
azarashi <azarashi@majestaria.fun>
2022-10-19 09:06:06 -0400

    Move files into new server package, update dependencies

--- server/minigames.go
+++ server/minigames.go
@@ -15,7 +15,7 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-package main
+package server
 
 type MinigameConfig struct {
 	MinigameId     string `json:"minigameId"`
