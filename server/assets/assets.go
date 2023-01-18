fc67d371e6ae1790216d951eeb9b5bd6c8c376be
maru <maru@myyahoo.com>
2023-01-18 13:14:27 -0500

    Update Yume 2kki sprite whitelist

--- server/assets/assets.go
+++ server/assets/assets.go
@@ -159,17 +159,30 @@ func (a *Assets) IsValidPicture(name string) bool {
 }
 
 func (a *Assets) IsValid2kkiSprite(name string, room int) bool {
-	if !(strings.Contains(name, "syujinkou") ||
-		strings.Contains(name, "effect") ||
-		strings.Contains(name, "yukihitsuji_game") ||
-		strings.Contains(name, "zenmaigaharaten_kisekae") ||
-		strings.Contains(name, "主人公") ||
-		name == "kodomo_04-1" ||
-		name == "RioCharset16" ||
-		name == "#null") ||
-		strings.Contains(name, "zenmaigaharaten_kisekae") && room != 176 {
-		return false
+	if (allowed2kkiSprites[name] ||
+		(strings.Contains(name, "syujinkou") ||
+			strings.Contains(name, "effect") ||
+			strings.Contains(name, "yukihitsuji_game") ||
+			strings.Contains(name, "zenmaigaharaten_kisekae") ||
+			strings.Contains(name, "主人公"))) &&
+		!(strings.Contains(name, "zenmaigaharaten_kisekae") && room != 176) {
+		return true
 	}
 
 	return true
+}
+
+var allowed2kkiSprites = map[string]bool{
+	"#null":                   true,
+	"kodomo_04-1":             true,
+	"Kong_Urotsuki_CharsetFC": true,
+	"kura CharSet01":          true,
+	"kuro9-8":                 true,
+	"natl_char_uro":           true,
+	"nuls_sujinkou":           true,
+	"RioCharset16":            true,
+	"urotsuki_sniper":         true,
+	"urotsuki_Swimsuit01":     true,
+	"urotsuki_Swimsuit02":     true,
+	"urotsuki_taoru":          true,
 }
