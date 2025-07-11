60095ae5f0501601a1c540d2d740b404a39c2595
r00 <30438415+zebraed@users.noreply.github.com>
2025-07-11 11:32:55 +0900

    Improve privacy checks for friend and party member location sharing (#65)
    * add privacy flag checks for sharing friend and party member location info
    
    * fix friend location sharing condition
    
    * reset party member location when location sharing is disabled and single player mode is enabled

--- server/party.go
+++ server/party.go
@@ -133,12 +133,18 @@ func getPartyData(partyId int) (*Party, error) {
 		member.Badge = client.badge
 		member.Medals = client.medals
 
-		if client.roomC != nil {
+		if client.roomC != nil && !(client.hideLocation && client.singleplayer) {
 			member.MapId = client.roomC.mapId
 			member.PrevMapId = client.roomC.prevMapId
 			member.PrevLocations = client.roomC.prevLocations
 			member.X = client.roomC.x
 			member.Y = client.roomC.y
+		} else if client.roomC != nil && (client.hideLocation && client.singleplayer) {
+			member.MapId = "0000"
+			member.PrevMapId = "0000"
+			member.PrevLocations = ""
+			member.X = 0
+			member.Y = 0
 		}
 
 		member.Online = true
