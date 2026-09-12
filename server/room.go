fbfd79e33bc4b440c5914ce0dcd1798f9e49df12
jackssrt <33695699+jackssrt@users.noreply.github.com>
2026-09-13 00:00:41 +0300

    fix(room): allow 0 as the first packet counter
    Signed-off-by: Colourless <205353678+AcrylonitrileButadieneStyrene@users.noreply.github.com>

--- server/room.go
+++ server/room.go
@@ -86,9 +86,13 @@ func joinRoomWs(conn *websocket.Conn, r *http.Request) {
 	}
 
 	client := &RoomClient{
-		conn:   conn,
-		outbox: make(chan []byte, 256),
-		key:    serverSecurity.NewClientKey(),
+		conn:    conn,
+		outbox:  make(chan []byte, 256),
+		key:     serverSecurity.NewClientKey(),
+		// 2^32-1, the biggest number uint32 can hold
+		// will overflow to 0 when incremented to check the counter (cnt + 1)
+		// allowing 0 to be the first packet counter
+		counter: 4294967295,
 	}
 
 	if session, ok := clients.LoadOK(pd.Uuid); ok {
